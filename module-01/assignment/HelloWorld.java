7e1ebd9fb5561f25acc49eeb1a81682074d4433a
SyntaxXSamurai <syntaxXsamurai@gmail.com>
2023-06-17 14:36:08 +0000

    First Git commit from visual studio code

--- module-01/assignment/HelloWorld.java
+++ module-01/assignment/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Print out "Hello World"
         System.out.println("Hello World");
+        // First Git commit from visual studio code
 	}
 }
